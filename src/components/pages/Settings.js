31302c93c63ea775f073bbc0408043ca2ee25e8b
kethelyn cavalari <155905688+Kethelynl@users.noreply.github.com>
2025-02-04 14:22:51 -0300

    Update Settings.js

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -8,43 +8,43 @@ export const Settings = ({texts}) =>{
                 <div className="img-icon-habiliti">
                     <div className="img-container">
                         <span>
-                        <img src="/assets/img/python.png"/>
+                        <img src="/assets/img/python.png" alt="Descrição da imagem"/>
                         <p>PYTHON</p>
                         </span>
                     </div>
                     <div className="img-container">
                         <span>
-                            <img src="/assets/img/javascript.png"/>
+                            <img src="/assets/img/javascript.png" alt="Descrição da imagem"/>
                             <p>JAVASCRIPT</p>
                         </span>
                     </div>
                     <div className="img-container">
                         <span>
-                        <img src="/assets/img/django.png"/>
+                        <img src="/assets/img/django.png" alt="Descrição da imagem"/>
                         <p>DJANGO</p>
                         </span>
                     </div>
                     <div className="img-container react">
                         <span>
-                        <img src="/assets/img/atom.png"/>
+                        <img src="/assets/img/atom.png" alt="Descrição da imagem"/>
                         <p>REACT</p>
                         </span>
                     </div>
                     <div className="img-container">
                         <span>
-                        <img src="/assets/img/html.png"/>
+                        <img src="/assets/img/html.png" alt="Descrição da imagem"/>
                         <p>HTML</p>
                         </span>
                     </div>
                     <div className="img-container">
                         <span>
-                        <img src="/assets/img/css.png"/>
+                        <img src="/assets/img/css.png" alt="Descrição da imagem"/>
                         <p>CSS</p>
                         </span>
                     </div>
                     <div className="img-container">
                         <span>
-                        <img src="/assets/img/git.png"/>
+                        <img src="/assets/img/git.png" alt="Descrição da imagem"/>
                         <p>GIT</p>
                         </span>
                     </div>
