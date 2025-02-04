e8619d99925c2ad7eed95279b4896b679fa1ef2a
kethelyn cavalari <155905688+Kethelynl@users.noreply.github.com>
2025-02-04 14:23:58 -0300

    Update Footer.js

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -12,8 +12,8 @@ export const Footer = () => {
             <Col size={12} sm={6} className="text-center text-sm-end">
                 <div className="social-icon">
                 <a href="https://www.linkedin.com/in/kethelyn-cavalari/"><img src="/assets/img/nav-icon1.svg" alt="Ícone 1" /></a>
-                <a href="https://github.com/Kethelynl"><img  src={icon2}/></a>
-                <a href="https://www.instagram.com/ket_cavalari/"><img  src="/assets/img/nav-icon3.svg"/></a>
+                <a href="https://github.com/Kethelynl"><img  src={icon2} alt="Descrição da imagem"/></a>
+                <a href="https://www.instagram.com/ket_cavalari/"><img  src="/assets/img/nav-icon3.svg" alt="Descrição da imagem"/></a>
                 </div>
                 <p>Copyright 2025. All Rights Reserved</p>
             </Col>
